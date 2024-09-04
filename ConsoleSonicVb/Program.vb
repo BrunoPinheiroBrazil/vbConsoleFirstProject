b8254df44d71da28cc2ac4d7b0704cca24f9a6af
BrunoPinheiroBrazil <brunopinheirobrazil@gmail.com>
2024-09-04 19:52:30 -0300

    atualizei a lógica do projeto

--- ConsoleSonicVb/Program.vb
+++ ConsoleSonicVb/Program.vb
@@ -2,10 +2,10 @@ Module Program
   Sub Main(args As String())
     Dim resposta = ""
 
-    While resposta <> "sair"
+    Console.WriteLine("Insira um nome ou insira sair para sair do programa:")
+    Dim entrada = Console.ReadLine()
 
-      Console.WriteLine("Insira um nome por favor!")
-      Dim entrada = Console.ReadLine()
+    While resposta <> "sair"
 
       If entrada = "Sonic" Then
         Console.WriteLine($"Parabéns {entrada} passou na entrevista!!")
@@ -18,6 +18,12 @@ Module Program
       If entrada = "Leia" Then
         Console.WriteLine($"A {entrada} está muito feliz que o Sonic vai ser programador VB Net!!!!")
       End If
+
+      Console.WriteLine()
+
+      Console.WriteLine("Insira outro nome por favor:")
+      entrada = Console.ReadLine()
+
     End While
 
   End Sub
